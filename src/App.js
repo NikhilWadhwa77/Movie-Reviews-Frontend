8613b3721d9ab2fc4b861f8303efc37c39c2b64d
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2024-02-06 00:13:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
     const getMovies = async () => {
         try {
-            const res = await fetch('/api/v1/movies', {
+            const res = await fetch('https://movies-backend-ctx9.onrender.com/api/v1/movies', {
                 method: 'GET'
             })
             const data = await res.json()
@@ -29,7 +29,7 @@ function App() {
     const getMovieData = async (movieId) => {
 
         try {
-            const res = await fetch(`/api/v1/movies/${movieId}`, {
+            const res = await fetch(`https://movies-backend-ctx9.onrender.com/api/v1/movies/${movieId}`, {
                 method: 'GET'
             })
             const singleMovie = await res.json()
